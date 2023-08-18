abf33da72b758f3982d83e62dc0eb7f799b6a530
Eduardo Fanis <eduardo.fanis@hotmail.com>
2023-08-17 21:27:01 -0300

    window error

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -5,20 +5,27 @@ import Image from "next/image";
 import React from "react";
 
 export default function Conta() {
-  const avatar = window ? window.localStorage.getItem("avatar") : null;
-  const nome = window ? window.localStorage.getItem("nome") : null;
+  if (typeof window !== "undefined") {
+    const avatar = window ? window.localStorage.getItem("avatar") : null;
+    const nome = window ? window.localStorage.getItem("nome") : null;
 
-  return (
-    <ProtectedRoute>
-      <div className="sm:w-96 ">
-        <Header account="true">Sua conta</Header>
-        <div className="grid justify-center text-center">
-          {avatar && (
-            <Image src={avatar} alt="Foto de perfil" width={128} height={128} />
-          )}
-          <h2 className="mt-5 text-xl">{nome}</h2>
+    return (
+      <ProtectedRoute>
+        <div className="sm:w-96 ">
+          <Header account="true">Sua conta</Header>
+          <div className="grid justify-center text-center">
+            {avatar && (
+              <Image
+                src={avatar}
+                alt="Foto de perfil"
+                width={128}
+                height={128}
+              />
+            )}
+            <h2 className="mt-5 text-xl">{nome}</h2>
+          </div>
         </div>
-      </div>
-    </ProtectedRoute>
-  );
+      </ProtectedRoute>
+    );
+  }
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,37 +6,40 @@ import Image from "next/image";
 
 export default function Header({ children, home, account }: any) {
   const User = React.useContext(UserContext);
-  const avatar = window ? window.localStorage.getItem("avatar") : null;
 
-  if (User && User.login === true) {
+  if (typeof window !== "undefined") {
+    const avatar = window.localStorage.getItem("avatar");
+
+    if (User && User.login === true) {
+      return (
+        <div className="flex w-full border-solid border-b border-zinc-900 pb-5 mb-10 justify-center place-content-between relative">
+          {!home && (
+            <Link href="/" className="absolute left-0">
+              <House size={20} color="#ccc" />
+            </Link>
+          )}
+          <h1 className="text-zinc-400">{children}</h1>
+          {!account && avatar ? (
+            <Link href="/conta" className="absolute right-0">
+              <Image src={avatar} alt="Foto de perfil" width={24} height={24} />
+            </Link>
+          ) : (
+            <button
+              onClick={() => User.userLogout()}
+              className="absolute right-0"
+            >
+              <SignOut size={20} color="red" />
+            </button>
+          )}
+        </div>
+      );
+    }
     return (
-      <div className="flex w-full border-solid border-b border-zinc-900 pb-5 mb-10 justify-center place-content-between relative">
-        {!home && (
-          <Link href="/" className="absolute left-0">
-            <House size={20} color="#ccc" />
-          </Link>
-        )}
+      <div className="flex w-full justify-center border-solid border-b border-zinc-900 pb-5">
         <h1 className="text-zinc-400">{children}</h1>
-        {!account && avatar ? (
-          <Link href="/conta" className="absolute right-0">
-            <Image src={avatar} alt="Foto de perfil" width={24} height={24} />
-          </Link>
-        ) : (
-          <button
-            onClick={() => User.userLogout()}
-            className="absolute right-0"
-          >
-            <SignOut size={20} color="red" />
-          </button>
-        )}
       </div>
     );
   }
-  return (
-    <div className="flex w-full justify-center border-solid border-b border-zinc-900 pb-5">
-      <h1 className="text-zinc-400">{children}</h1>
-    </div>
-  );
 }
 
 export function HeaderLogin({ children }: any) {
